2808cee5a00b3059516774d6f46bd4e7ef2e4f10
mike.zhu <512122313@qq.com>
2022-08-13 16:32:48 +0800

    demo

--- main.js
+++ main.js
@@ -4,19 +4,28 @@ class MyComponent extends Component {
     constructor() {
         super();
     }
+    state = { value: 1 };
     render() {
         return <div>
             {this.attributes.text}
+
+            <div>state: {this.state.value}</div>
             {this.children}
+            <Button onClick={() => { this.setState({ value: this.state.value + 1 }) }}>click</Button>
         </div>
+    }
+}
+class Button extends Component {
+    render() {
+        return <button >{this.children}</button>
     }
 }
 let jsx = <div id='container'>
     whatever i am
-    <MyComponent id='s' text='sth'>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+    <MyComponent id='s' text='some text'>
+        <div>child1</div>
+        <div>child2</div>
+        <div>child3</div>
     </MyComponent>
 </div>
 
