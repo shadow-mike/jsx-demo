3a971950d4e4328d90424b500441b010d26ef112
mike.zhu <512122313@qq.com>
2022-08-13 12:58:59 +0800

    init MyComponent

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function createElement(type, attributes, ...children) {
-    const ele = document.createElement(type);
+    const ele = typeof type === 'string' ? document.createElement(type) : new type;
 
     for (let name in attributes) {
         ele.setAttribute(name, attributes[name])
@@ -8,16 +8,28 @@ function createElement(type, attributes, ...children) {
         if (typeof child === 'string') {
             child = document.createTextNode(child);
         }
-        ele.appendChild(child);
+        if (child.appendTo) {
+            child.appendTo(ele);
+        } else {
+            ele.appendChild(child); // child不是Node类的话会报错
+        }
     }
     return ele;
 }
+
+class MyComponent {
+    constructor() {
+        console.log('MyComponent');
+    }
+    setAttribute(key, val) {
+
+    };
+    appendChild(child) { }
+    appendTo(parent) { }
+}
 let jsx = <div id='container'>
-    test2
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
+    whatever i am
+    <MyComponent id='s'></MyComponent>
 </div>
 
 document.body.appendChild(jsx);
