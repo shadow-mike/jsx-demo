5f4d477967c944c464f15fa70987880e0a8d7422
mike.zhu <512122313@qq.com>
2022-08-13 16:08:48 +0800

    抽离成Component类

--- main.js
+++ main.js
@@ -1,68 +1,10 @@
-function createElement(type, attributes, ...children) {
-    const ele = typeof type === 'string' ? document.createElement(type) : new type;
+import { Component, createElement } from './lib.js'
 
-    for (let name in attributes) {
-        ele.setAttribute(name, attributes[name])
-    }
-
-    const renderChildren = (children) => {
-        for (let child of children) {
-            if (typeof child === 'string') {
-                child = document.createTextNode(child);
-            }
-            if (Array.isArray(child)) {
-                renderChildren(child);
-                continue;
-            }
-            if (child.mount) {
-                child.mount(ele);
-            } else {
-                ele.appendChild(child); // child不是Node类的话会报错
-            }
-        }
-    };
-    renderChildren(children);
-    return ele;
-}
-
-class MyComponent {
+class MyComponent extends Component {
     constructor() {
-        console.log('MyComponent');
-        this.root = document.createElement('div');
-        this.renderQuest = false;
-        this.children = [];
-        this.attributes = {};
-        this.requestRender();
-    }
-    setAttribute(name, val) {
-        // this.root.setAttribute(name, val);
-        this.attributes[name] = val;
-        this.requestRender();
-    };
-    appendChild(child) {
-        this.children.push(child);
-        this.requestRender();
-    }
-    mount(parent) {
-        parent.appendChild(this.root);
-        this.requestRender();
-    }
-    requestRender() {
-        if (this.renderQuest) return;
-        this.renderQuest = true;
-        // start a async render task
-        Promise.resolve().then(() => {
-            this.renderQuest = false;
-            this.root.innerHTML = '';
-            this.root.appendChild(this.render());
-        })
+        super();
     }
     render() {
-        console.log('rendering');
-        // this.root.appendChild(document.createTextNode(this.root.getAttribute('text')));
-        // for (let child of this.children) {
-        //     this.root.appendChild(child);
-        // }
         return <div>
             {this.attributes.text}
             {this.children}
