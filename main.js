24d4984c749ac675d696085fa5a9a5f978566f24
mike.zhu <512122313@qq.com>
2022-08-13 14:09:43 +0800

    MyComponent, requestRender

--- main.js
+++ main.js
@@ -21,20 +21,47 @@ class MyComponent {
     constructor() {
         console.log('MyComponent');
         this.root = document.createElement('div');
+        this.renderQuest = false;
+        this.children = [];
+        this.requestRender();
     }
     setAttribute(name, val) {
         this.root.setAttribute(name, val);
+        this.requestRender();
     };
     appendChild(child) {
-        this.root.appendChild(child);
+        this.children.push(child);
+        this.requestRender();
     }
     mount(parent) {
         parent.appendChild(this.root);
+        this.requestRender();
+    }
+    requestRender() {
+        if (this.renderQuest) return;
+        this.renderQuest = true;
+        // start a async render task
+        Promise.resolve().then(() => {
+            this.renderQuest = false;
+            this.render();
+        })
+    }
+    render() {
+        console.log('rendering');
+        this.root.innerHTML = '';
+        this.root.appendChild(document.createTextNode(this.root.getAttribute('text')));
+        for (let child of this.children) {
+            this.root.appendChild(child);
+        }
     }
 }
 let jsx = <div id='container'>
     whatever i am
-    <MyComponent id='s'></MyComponent>
+    <MyComponent id='s' text='sth'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+    </MyComponent>
 </div>
 
 document.body.appendChild(jsx);
